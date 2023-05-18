a025015670163eee9562a57d7ddfda52b7e03905
mayowa307 <131859206+mayowa307@users.noreply.github.com>
2023-05-18 10:05:03 +0100

    Add files via upload

---
+++ gittest/calculator/new.py
@@ -0,0 +1,56 @@
+myname = 'akintunde' #STRING
+myage = 21 #integer
+height = 36.1 #float
+is_approved = True #boolean
+pin = 2334
+
+
+#input and print
+
+# print(myage)
+# name = input('what is your name? ')
+
+# print('Good evening ' + name)
+
+# a = 145
+# b = 2
+
+# c = a / b
+# d = a // b
+# e = a % b #remainder
+# print(c)
+# print(d)
+# print(e)
+
+
+# no_of_people = input('how many people are you sharing the bill with? ') 
+# total_amount = input('how much is the total bill? ')
+
+# split_bill = int(total_amount) / int(no_of_people)
+# print('each person is to pay $' + str(split_bill)) 
+
+
+amount = int(input('how much are you getting?'))
+first = input('what is your name? ')
+second = input('what is your name? ')
+
+percent60 = 60/100
+percent40 = 40/100
+
+first_amount = percent60 * amount
+second_amount = percent40 * amount
+
+print(first + ' would get ' + str(first_amount))
+print(second + ' would get ' + str(second_amount))
+
+
+
+
+
+
+
+
+
+
+
+
